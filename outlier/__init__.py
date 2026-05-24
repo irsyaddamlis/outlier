ea48e9ebd1d34e758c3e8f8470970f806823c78a
Irsyad Damlis <irsyad.damlis@gmail.com>
2026-05-24 17:51:53 +0700

    revise library command import

--- outlier/__init__.py
+++ outlier/__init__.py
@@ -1,14 +1,22 @@
 from importlib.metadata import version, PackageNotFoundError
+from .detector import OutlierDetector
+import sys
 
+# --- Metadata ---
 try:
-    __version__ = version("outlier")    # auto-reads from pyproject.toml
+    __version__ = version("outlier")
 except PackageNotFoundError:
-    __version__ = "unknown"             # fallback if not installed yet
+    __version__ = "unknown"
 
 __author__ = "Irsyad Damlis"
 __email__ = "irsyad.damlis@gmail.com"
 __license__ = "arch-notions"
 
-from .detector import OutlierDetector
-
 __all__ = ["OutlierDetector"]
+
+# --- Make module callable: outlier(data) ---
+class _CallableModule(sys.modules[__name__].__class__):
+    def __call__(self, *args, **kwargs):
+        return OutlierDetector(*args, **kwargs)
+
+sys.modules[__name__].__class__ = _CallableModule
